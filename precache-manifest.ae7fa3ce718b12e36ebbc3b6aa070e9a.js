3f58a1d3fbcf5cd15d3262035ff6dd6c7d208c95
vaz007 <ankur@adaptideations.com>
2019-11-19 23:22:47 +1100

    Updates

---
+++ precache-manifest.ae7fa3ce718b12e36ebbc3b6aa070e9a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab67c98183ce311cf581b51c449ce387",
+    "url": "/curriculum_vitae/index.html"
+  },
+  {
+    "revision": "d44f85cbf4c907b9a771",
+    "url": "/curriculum_vitae/static/css/2.6652cf7a.chunk.css"
+  },
+  {
+    "revision": "7a1e9ef54ae83c5a3523",
+    "url": "/curriculum_vitae/static/css/main.037a827f.chunk.css"
+  },
+  {
+    "revision": "d44f85cbf4c907b9a771",
+    "url": "/curriculum_vitae/static/js/2.a575b6dd.chunk.js"
+  },
+  {
+    "revision": "7a1e9ef54ae83c5a3523",
+    "url": "/curriculum_vitae/static/js/main.7641b386.chunk.js"
+  },
+  {
+    "revision": "23c13e67721ef4acaba8",
+    "url": "/curriculum_vitae/static/js/runtime-main.c739497c.js"
+  },
+  {
+    "revision": "9628891c3e19d3340620aec02569a2b2",
+    "url": "/curriculum_vitae/static/media/AboutPage.9628891c.png"
+  },
+  {
+    "revision": "47e9e25619522e31ed6d644ad71ec378",
+    "url": "/curriculum_vitae/static/media/ankurThumbNail.47e9e256.jfif"
+  },
+  {
+    "revision": "d20d95f49b96c621eeb1a2359ca8d1ed",
+    "url": "/curriculum_vitae/static/media/banner.d20d95f4.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/curriculum_vitae/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/curriculum_vitae/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/curriculum_vitae/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/curriculum_vitae/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/curriculum_vitae/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/curriculum_vitae/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/curriculum_vitae/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/curriculum_vitae/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/curriculum_vitae/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/curriculum_vitae/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/curriculum_vitae/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/curriculum_vitae/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/curriculum_vitae/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/curriculum_vitae/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/curriculum_vitae/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "a83ea39be1eb263667c02f9cf63fc1f3",
+    "url": "/curriculum_vitae/static/media/ipadBanner.a83ea39b.png"
+  }
+]);
